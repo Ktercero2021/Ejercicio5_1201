82ac47cc285eb83a9cfa96f75ad79e1f85f38db4
Ktercero2021 <86389488+Ktercero2021@users.noreply.github.com>
2021-07-01 16:51:13 -0600

    primer commit
    primer commit del programa5

---
+++ Ejercicio5_1201/Ejercicio5_1201/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio5_1201
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_ejecutar_Click(object sender, EventArgs e)
+        {
+
+            int[,] matriz = new int[Convert.ToInt32(textBox1_filas.Text), Convert.ToInt32(textBox2_columnas.Text)];
+
+            Random matrizAleatoria = new Random();
+
+
+            for ( int filas = 0; filas < matriz.GetLength(0); filas++)
+            {
+                for(int columna =0; columna< matriz.GetLength(1); columna++)
+                {
+
+                    matriz[filas, columna] = matrizAleatoria.Next(0, 100);
+
+                    MessageBox.Show("La posición es:" + filas + " , " + columna + " = " + matriz[filas, columna].ToString());
+                }
+
+            } 
+        }
+    }
+}
